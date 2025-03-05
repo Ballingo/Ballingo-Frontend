f0894549a8bbfb836a8fb007956aec6ac9505a46
Santiago Santana Martínez <pimisan@gmail.com>
2025-03-05 02:01:49 +0000

    feat(Pet): You can now feed your pet

--- components/hunger-bar/HungerBar.tsx
+++ components/hunger-bar/HungerBar.tsx
@@ -1,19 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Image } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import styles from "./HungerBarStyles";
+import { getHungerBar } from "@/api/pet_api";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 interface HungerBarProps {
-  hungerLevel: number;
   width: number;
 }
 
-const HungerBar: React.FC<HungerBarProps> = ({ hungerLevel, width }) => {
+const HungerBar: React.FC<HungerBarProps> = ({ width }) => {
+  const [hungerBar, setHungerBar] = useState<number>(100);
+  
+  useEffect(() => {
+    const fetchHungerBar = async () => {
+      const petId = await AsyncStorage.getItem("PetId");
+
+      if (!petId){
+        console.error("❌ Couldn't get find pet");
+        return
+      }
+
+      const {data, status} = await getHungerBar(petId);
+
+      if (status === 200) {
+        console.log("✅ Hunger bar data:", data);
+        setHungerBar(data.hunger);
+      }
+      else {
+        console.error("❌ Error getting hunger bar:", data);
+      }
+
+    };
+    fetchHungerBar();
+  }, []);
+
   return (
     <View style={[styles.container, { width: `${width}%` }]}>
       <Image
         source={require("./assets/chicken.png")}
-        style={[styles.icon, { left: `${hungerLevel}%` }]}
+        style={[styles.icon, { left: `${hungerBar}%` }]}
       />
 
       <View style={styles.barBackground}>
@@ -24,7 +50,7 @@ const HungerBar: React.FC<HungerBarProps> = ({ hungerLevel, width }) => {
           style={styles.gradient}
         />
 
-        <View style={[styles.barFill, { width: `${100 - hungerLevel}%` }]} />
+        <View style={[styles.barFill, { width: `${100 - hungerBar}%` }]} />
       </View>
     </View>
   );
